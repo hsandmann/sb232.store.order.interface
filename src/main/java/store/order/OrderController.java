c101cb70a8fc2bd835fa60a145f2db41a6457620
Humberto Sandmann <hsandmann@ieee.org>
2023-12-02 00:39:13 -0300

    session id-user

--- src/main/java/store/order/OrderController.java
+++ src/main/java/store/order/OrderController.java
@@ -9,20 +9,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient("store-order")
 public interface OrderController {
 
     @GetMapping("/order/")
-    public List<OrderOut> list();
+    public List<OrderOut> list(
+        @RequestHeader(value = "id-user", required = true) String idUser
+    );
 
     @GetMapping("/order/{id}")
-    public OrderOut get(@PathVariable(required = true) String id);
+    public OrderOut get(
+        @RequestHeader(value = "id-user", required = true) String idUser,        
+        @PathVariable(required = true) String id
+    );
 
     @DeleteMapping("/order/{id}")
-    public void delete(@PathVariable(required = true) String id);
+    public void delete(
+        @RequestHeader(value = "id-user", required = true) String idUser,        
+        @PathVariable(required = true) String id
+    );
 
     @PostMapping("/order/")
-    public ResponseEntity<Object> create(@RequestBody OrderIn in);
+    public ResponseEntity<Object> create(
+        @RequestHeader(value = "id-user", required = true) String idUser,        
+        @RequestBody OrderIn in
+    );
 
 }
